e94e403dea7508e76cea373ea61befbfb634e556
baghbaghooo <mustafa864433@gmail.com>
2026-03-14 06:36:23 +0330

    Update import path for internal binary package

--- internal/handlers/client_run.go
+++ internal/handlers/client_run.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	"github.com/net2share/nethopper/internal/actions"
-	"github.com/net2share/nethopper/internal/binary"
+	"github.com/baghbaghooo/nethopper/internal/binary"
 	"github.com/net2share/nethopper/internal/config"
 	"github.com/net2share/nethopper/internal/network"
 	"github.com/net2share/nethopper/internal/service"
